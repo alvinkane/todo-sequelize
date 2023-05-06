a1290f0b08b17a43ee68b6942026aae775961d60
alvin <ckuvn1854@gmail.com>
2023-05-06 15:43:13 +0800

    feat: add login, register post and logout routes

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,8 +5,20 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.post("/login", (req, res) => {
+  res.send("login");
+});
+
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  res.send("register");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("logout");
 });
 
 module.exports = router;
