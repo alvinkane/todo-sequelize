78e57f7ad4cb4cec18860623b51eee3325f67779
alvin <ckuvn1854@gmail.com>
2023-05-07 13:50:47 +0800

    feat: add home page and read database's data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+const db = require("../../models");
+const Todo = db.Todo;
+const User = db.User;
+
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.findAll({ raw: true, nest: true });
+    res.render("index", { todos });
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 module.exports = router;
