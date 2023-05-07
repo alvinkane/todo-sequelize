e5b623c4e60e7a73ab4ffcadca9f30885d979fae
alvin <ckuvn1854@gmail.com>
2023-05-07 12:09:35 +0800

    feat: seeder use async/await

--- seeders/20230506131732-default-data.js
+++ seeders/20230506131732-default-data.js
@@ -6,10 +6,52 @@ const SEED_USER = {
   password: "12345678",
 };
 
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface
+//       .bulkInsert(
+//         "Users",
+//         [
+//           {
+//             name: SEED_USER.name,
+//             email: SEED_USER.email,
+//             password: bcrypt.hashSync(
+//               SEED_USER.password,
+//               bcrypt.genSaltSync(10),
+//               null
+//             ), //同步的作法
+//             createdAt: new Date(),
+//             updatedAt: new Date(),
+//           },
+//         ],
+//         {}
+//       )
+//       .then((userId) => {
+//         console.log(userId);
+//         queryInterface.bulkInsert(
+//           "Todos",
+//           Array.from({ length: 10 }).map((_, i) => ({
+//             name: `name-${i}`,
+//             UserId: userId,
+//             createdAt: new Date(),
+//             updatedAt: new Date(),
+//           }))
+//         );
+//       });
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface
+//       .bulkDelete("Todos", null, {})
+//       .then(() => queryInterface.bulkDelete("Users", null, {}));
+//   },
+// };
+
+// async/await版本
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface
-      .bulkInsert(
+  async up(queryInterface, Sequelize) {
+    try {
+      const userId = await queryInterface.bulkInsert(
         "Users",
         [
           {
@@ -25,23 +67,26 @@ module.exports = {
           },
         ],
         {}
-      )
-      .then((userId) =>
-        queryInterface.bulkInsert(
-          "Todos",
-          Array.from({ length: 10 }).map((_, i) => ({
-            name: `name-${i}`,
-            UserId: userId,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          }))
-        )
       );
+      await queryInterface.bulkInsert(
+        "Todos",
+        Array.from({ length: 10 }).map((_, i) => ({
+          name: `name-${i}`,
+          UserId: userId,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }))
+      );
+    } catch (error) {
+      throw err;
+    }
   },
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface
-      .bulkDelete("Todos", null, {})
-      .then(() => queryInterface.bulkDelete("Users", null, {}));
+  async down(queryInterface, Sequelize) {
+    try {
+      await queryInterface.bulkDelete("Todos", null, {});
+      await queryInterface.bulkDelete("Users", null, {});
+    } catch (error) {
+      throw err;
+    }
   },
 };
